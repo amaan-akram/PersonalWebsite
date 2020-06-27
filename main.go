3582c21b47bce6d1f9110271830ed06a5192595f
amaan-akram <amaan7akram@gmail.com>
2020-06-27 22:33:23 +0100

    go stuff?

--- main.go
+++ main.go
@@ -4,8 +4,20 @@ import "gopkg.in/macaron.v1"
 
 func main() {
     m := macaron.Classic()
-    m.Get("/", func() string {
-        return "Hello world!"
-    })
+    m.Get("/", HomepageHandler) 
+        
+    
     m.Run()
+}
+
+
+// NotFoundHandler handles 404 errors
+func NotFoundHandler(ctx *macaron.Context) {
+	ctx.HTML(http.StatusNotFound, "notfound")
+}
+
+// DevicesHandler handles the devices page
+func HomepageHandler(ctx *macaron.Context) {
+	ctx.Data["PageTitle"] = ""
+	ctx.HTML(http.StatusOK, "home")
 }
